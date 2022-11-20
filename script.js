3484c8b24ae51bb7e3c82df04ff6ecd087fc5490
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2022-11-20 15:20:06 +0200

    change quote content dynamiclly and add loading spinner

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
 const apiURL = 'https://type.fit/api/quotes';
 
-let quotesData = []
+const quoteContainer = document.querySelector("#quote__container")
+const quoteText = document.querySelector('.quote--content__text');
+const quoteAuthor = document.querySelector('.quote--author h5');
+const loadingSpinner = document.querySelector("#loading__spinner")
+
+let quotesData = [];
+
+function showLoadingSpinner() {
+    loadingSpinner.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+    if(!loadingSpinner.hidden) {
+        loadingSpinner.hidden = true;
+        quoteContainer.hidden = false;
+    }
+}
 
 function getNewQuote() {
+    showLoadingSpinner()
     const quote = quotesData[Math.floor(Math.random() * quotesData.length)];
-    console.log(quote)
+    if(quote.text.length > 50) {
+        quoteText.classList.add("quote--content__long")
+    } else {
+        quoteText.classList.remove('quote--content__long')
+    }
+    if(!quote.author) {
+        quoteAuthor.textContent = "Unknown"
+    } else {
+        quoteAuthor.textContent = quote.author
+    }
+    quoteText.textContent = quote.text;
+    removeLoadingSpinner()
 }
 
 async function fetchQuotes() {
+    showLoadingSpinner()
     try {    
         const response = await fetch(apiURL);
         quotesData = await response.json();
